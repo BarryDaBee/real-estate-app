3fe22d05cbf35ad054b04e8c0cf9e51f5b0fa363
Olusesi Barry <bolexido192@gmail.com>
2025-02-21 07:29:02 +0100

    feature(font): changed app font from Inter to Euclid Circular

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:real_estate_app/gen/fonts.gen.dart';
 import 'package:real_estate_app/src/app/app_router/app_router.dart';
 
 void main() {
@@ -25,6 +26,7 @@ class RealEstateApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
+          fontFamily: FontFamily.euclidCircularA,
         ),
       ),
     );
