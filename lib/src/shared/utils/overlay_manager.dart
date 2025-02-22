0801d17497bfaf6afb758af84f86d4cf7be4f491
Olusesi Barry <bolexido192@gmail.com>
2025-02-22 06:09:44 +0100

    feature(app): created overlay manager

---
+++ lib/src/shared/utils/overlay_manager.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class OverlayManager {
+  final List<OverlayEntry> _entries = [];
+
+  void addOverlay(OverlayState overlayState, OverlayEntry entry) {
+    overlayState.insert(entry);
+    _entries.add(entry);
+    _updatePositions();
+  }
+
+  void removeOverlay(OverlayEntry entry) {
+    entry.remove();
+    _entries.remove(entry);
+    _updatePositions();
+  }
+
+  List<OverlayEntry> get allEntries => List.unmodifiable(_entries);
+
+  void _updatePositions() {
+    for (var i = 0; i < _entries.length; i++) {
+      _entries[i].markNeedsBuild();
+    }
+  }
+}
